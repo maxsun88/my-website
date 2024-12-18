71fcf8be0d66ecfd4ec77e312c7d491226c186a7
maxsun88 <maxsunsny@gmail.com>
2024-12-18 14:05:17 -0500

    add files?

---
+++ .history/src/App_20241218130119.js
@@ -0,0 +1,67 @@
+import './App.scss';
+import { NavBar } from './components/NavBar/NavBar';
+import { FilmHomePage } from './pages/FilmHomePage/FilmHomePage';
+import { createBrowserRouter, RouterProvider, Outlet, Navigate, ScrollRestoration } from "react-router-dom";
+import { FilmProjectPage } from './pages/FilmProjectPage/FilmProjectPage';
+import { FilmJourneyPage } from './pages/FilmJourneyPage/FilmJourneyPage';
+import FJTheBeginning from './pages/FilmJourneyPage/film-journey.mdx';
+import FJSettingUpFilmClub from './pages/FilmJourneyPage/setting-up-a-filmmaking-club.mdx';
+import FJSecondShortFilm from './pages/FilmJourneyPage/second-short-film.mdx'
+import FJToronto from './pages/FilmJourneyPage/toronto.mdx'
+import FJForPetesSake from './pages/FilmJourneyPage/for-petes-sake.mdx'
+import FJMusical from './pages/FilmJourneyPage/musical.mdx'
+import { Footer } from './components/Footer/Footer';
+
+export const RootLayout = () => {
+  return (
+    <div>
+      <ScrollRestoration />
+      <NavBar />
+      <main className='content'>
+        <Outlet /> {/* This is where child routes will be rendered */}
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+  path: "/", // Root route
+    element: <RootLayout />, // This is where NavBar and Footer are rendered
+    children: [
+      {
+        path: "", // Matches the root URL
+        element: <FilmHomePage />,
+      },
+      {
+        path: "project/:projectId", // /projects/:projectId dynamic route
+        element: <FilmProjectPage />,
+      },
+      {
+        path: "journey", // /projects/:projectId dynamic route
+        element: <FilmJourneyPage />,
+        children: [
+          {
+            index: true, // Automatically match /journey path
+            element: <Navigate to="the-beginning" replace />, // Redirect to section1
+          },
+          { path: "the-beginning", element: <FJTheBeginning /> },
+          { path: "setting-up-a-film-club", element: <FJSettingUpFilmClub /> },
+          { path: "second-short-film", element: <FJSecondShortFilm /> },
+          { path: "toronto", element: <FJToronto /> },
+          { path: "for-petes-sake", element: <FJForPetesSake /> },
+          { path: "musical", element: <FJMusical /> },
+        ],
+      }
+    ],
+  },
+])
+
+function App() {
+  return (
+    <RouterProvider router={router} />
+  );
+}
+
+export default App;
---
+++ .history/src/contents/projectData_20241218124820.js
@@ -0,0 +1,65 @@
+export const projectData = {
+    'the-unknown-soldier': {
+        title: "The Unknown Soldier",
+        role: "Lead Producer, 1st AD",
+        lengthAndYear:"12 min, 2025",
+        // videoLink: "https://youtu.be/qBHwhgx487g",
+        intro: "This Still under construction",
+        storyUrl: 'the-beginning'
+    },
+    'ivd-ivw': {
+        title: "I've Been Dreaming, \nI've Been Working",
+        role: "Lead Producer",
+        lengthAndYear: "7 min, 2023",
+        intro: `This is my second time participating the 48 hour film competition as a producer. It's kinda unreal that we accomplished a musical short film within just 2 days! \n
+View the movie here (still in festival circulation):  https://t.ly/2qk11
+        `,
+        storyUrl: 'musical'
+    },
+    'for-petes-sake': {
+        title: "For Pete's Sake",
+        role: "Director, Producer",
+        lengthAndYear: "8 min, 2023",
+        videoLink: "https://vimeo.com/862226320",
+        intro: `For Pete’s Sake is a coming-of-age drama about a boy (LEE) who struggles to defeat his father in an arm wrestling match as he grows up. But when LEE finally achieves victory over his father, he realizes the bittersweet reality of his father’s dismissing strength.
+
+Moreover, This is a story that's based on real life events. The original story is written by Thomas Brodkin, who wrote the story based on his memory with his dad. I saw the story on an online writers' forum called Reedsy and I reached out to him for the permssion to adapt it to a short film.
+
+View the final film here: https://t.ly/6sXfY`,
+        storyUrl: 'for-petes-sake'
+    },
+    'lolas-recipe':{
+        title: "Lola's Recipe",
+        role: "Producer",
+        lengthAndYear: "3 min, 2023",
+        videoLink: "https://youtu.be/3MF6fmGeJRg",
+        intro: `A mother has to find the courage to tell her little daughter the passing of the grandma. What connects the family in the end is the beloved bakery recipe the grandma left. As the story is set in the filipino culture, Lola means exactly "grandma" in Tagalog. 
+
+This is a simple 3-minute short film as part of the #YODOContest, in which we proudly won the first place!`,
+        storyUrl: 'toronto'
+    },
+    'a-few-more': {
+        title: "A Few More",
+        role: "Producer",
+        lengthAndYear: "7 min, 2022",
+        videoLink: "https://youtu.be/lPwTspQ7Gak",
+        intro: `A Few More is a film made within 48 hours, from writing to editing. This was my first time participating in the 48 Hour Film Project in Toronto, and it marked the beginning of many more projects after.`,
+        storyUrl: 'toronto'
+    },
+    'latte-art': {
+        title: "Latte Art",
+        role: "Director, Producer, Writer & Editor",
+        lengthAndYear: "15 min, 2021",
+        videoLink: "https://vimeo.com/737726161",
+        intro: `Latte art is my second short film. Making it was a very tough journey from which I learnt awful a LOT!`,
+        storyUrl: 'second-short-film'
+    },
+    'sleepiness': {
+        title: "困 (Sleepiness)",
+        role: "Director, Producer, Writer & Editor",
+        lengthAndYear:"20 min, 2015",
+        videoLink: "https://youtu.be/qBHwhgx487g",
+        intro: "This is my first short film ever made, back in high school.",
+        storyUrl: 'the-beginning'
+    }
+}
